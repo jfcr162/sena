22e4ca991e4f4dc96c9fabe0bf5bd7e038603ffb
jfcr162 <corralesjhon509@gmail.com>
2024-07-19 19:18:31 -0500

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+let email;
+let password;
+const enviar = document.getElementById("enviar")
+
+email = document.getElementById("exampleInputEmail1")
+password = document.getElementById("exampleInputPassword1")
+
+
+    enviar.addEventListener("click",()=>{
+        email.value
+        password.value
+    })
+console.log(email, password)
